a6aab701e06595d61f7daed9ea8b8a58c8f6631e
SARTHAK SHARMA <141639566+Sarthak92101@users.noreply.github.com>
2025-03-15 10:58:29 +0530

    Add files via upload

---
+++ Simons Gane/app.js
@@ -0,0 +1,11 @@
+let ganeseq=[];
+let userseq=[];
+
+let started=false;
+let level=0;
+document.addEventListener("keypress", function(){
+  if(started=false){
+  console.log("Key pressed")
+   started=true;
+}
+})
